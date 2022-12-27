2204a6872df02692efb35e9175b9d441ceb73184
Oleg <evstifeevoleg1989@gmail.com>
2022-12-27 14:03:05 +0400

    Перед изменением стилей и вёрстки

--- app/js/main.js
+++ app/js/main.js
@@ -27,6 +27,7 @@ $( document ).ready(function() {
         Шаги в квизе
   ============================
    */
+
   $('#steps__button--next1').on('click', function () {
     if ($('input[name="steps-one"]').is(':checked')) {
       $('#step-stage1').removeClass('steps__stage--active')
@@ -102,7 +103,7 @@ $( document ).ready(function() {
   $('.noUi-tooltip').on('DOMSubtreeModified', function () {
     const rateVal = $(".noUi-tooltip").text();
 
-    $('.jstooltipNumber').val(rateVal ).attr("value", rateVal)
+    $('.jstooltipNumber').val(rateVal).attr("value", rateVal)
 
 
     if (rateVal >= 840) {
@@ -178,32 +179,35 @@ $( document ).ready(function() {
 
   $(".phone-mask").mask("+9(999) 999-9999");
 
+  /*
+  ==================================================
+     Убираем стандартное сообщение о валидации
+  ==================================================
+   */
+  $('input, select, textarea').on("invalid", function (e) {
+    e.preventDefault();
+  });
 
   /*
 ==========================================
    Страница спасибо при отправки формы
 ==========================================
  */
-  $('.steps__four-button').click(function () { // задаем функцию при нажатиии на элемент <button>
+  $('.steps__four-button').on('click', function () { // задаем функцию при нажатиии на элемент <button>
 
-      if ($('.steps__four-input').val().length === 0) {
-        $('.steps__four-label').addClass('warning');
+    if ($('.steps__four-input').val().length === 0) {
+      $('.steps__four-label').addClass('warning');
+      if (($('.phone-mask').val().length > 15)) {
+        $('.steps__four-label ').addClass('warning');
+        $('.steps__four-label--phone').removeClass('warning');
       }
-
-
-    else if ($('.name').val().length <= 1 ) {
+    } else if ($('.name').val().length <= 1) {
       $('.steps__four-label--phone').removeClass('warning');
       $('.steps__four-label--name').addClass('warning');
-    }
-
-
-    else if ($('.phone-mask').val().length <= 12) {
-        $('.steps__four-label').removeClass('warning');
-        $('.steps__four-label--phone').addClass('warning');
-      }
-
-
-    else {
+    } else if ($('.phone-mask').val().length <= 12) {
+      $('.steps__four-label').removeClass('warning');
+      $('.steps__four-label--phone').addClass('warning');
+    } else {
       $('.quiz__steps').submit(function (event) { // задаем функцию при срабатывании события "submit" на элементе <form>
         event.preventDefault(); // действие события по умолчанию не будет срабатывать
         $('.popup-thanks').css('display', 'flex').fadeIn();
@@ -212,7 +216,6 @@ $( document ).ready(function() {
       });
     }
   });
-
 
 
   $('.name').on('keypress', function () {
@@ -237,25 +240,207 @@ $( document ).ready(function() {
 
     let href = $(this).attr('href');
     $('html, body').animate({
-      scrollTop: $(href).offset().top  - 40 + 'px'
+      scrollTop: $(href).offset().top - 40 + 'px'
     }, {
       duration: 500,
       easing: "linear"
     });
   })
 
+// ==============================================
+// Табы во вкладке товары
+// ==============================================
 
-
-  $('.js-tab-trigger').click(function() {
+  $('.js-tab-trigger').click(function () {
     let id = $(this).attr('data-tab'),
-      content = $('.js-tab-content[data-tab="'+ id +'"]');
+      content = $('.js-tab-content[data-tab="' + id + '"]');
 
     $('.js-tab-trigger.tab-header__item--active').removeClass('tab-header__item--active'); // 1
     $(this).addClass('tab-header__item--active'); // 2
 
-    $('.js-tab-content.active').removeClass('active'); // 3
-    content.addClass('active'); // 4
+    $('.js-tab-content.tab-content__box--active').removeClass('tab-content__box--active').hide(); // 3
+    content.addClass('tab-content__box--active').fadeIn(); // 4
   });
 
 
-});
+// ==============================================
+// Табы карты
+// ==============================================
+  $('.js-map-trigger').click(function () {
+    let id = $(this).attr('data-tab'),
+      content = $('.js-map-content[data-tab="' + id + '"]');
+
+    $('.js-map-trigger.js-map-trigger-active').removeClass('map__card--active js-map-trigger-active'); // 1
+    $(this).addClass('map__card--active js-map-trigger-active'); // 2
+
+    $('.js-map-content.js-map-content-active').slideUp().removeClass('js-map-content-active'); // 3
+    content.slideDown().addClass('js-map-content-active').css('display', 'flex'); // 4
+  });
+
+
+// ==============================================
+// Яндекс карта скрипт
+// ==============================================
+  document.addEventListener('DOMContentLoaded', function () {
+    ymaps.ready(init);
+
+    function init() {
+      var myMap = new ymaps.Map('map', {
+          center: [55.776573, 37.573851],
+          zoom: 10
+        }),
+        objectManager = new ymaps.ObjectManager({
+          clusterize: true,
+          gridSize: 32
+        });
+      myMap.geoObjects.add(objectManager);
+      objectManager.objects.options.set('preset', 'islands#redDotIcon');
+      myMap.geoObjects.add(objectManager);
+      objectManager.add({
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            id: 1,
+            geometry: {
+              type: "Point",
+              coordinates: [55.776573, 37.573851]
+            },
+            properties: {
+              balloonContent: 'Скаковая улица, 36, 4 этаж, оф.423',
+              iconCaption: 'Москва Офис',
+            },
+            options: {
+              iconLayout: 'default#imageWithContent',
+              iconImageHref: 'https://liori.ru/wp-content/themes/theme-liori/img/pin.svg',
+              iconImageSize: [76, 76],
+              iconImageOffset: [-54, -54],
+              iconContentOffset: [15, 15],
+            }
+          },
+          {
+            type: "Feature",
+            id: 2,
+            geometry: {
+              type: "Point",
+              coordinates: [55.725595, 37.449677]
+            },
+            properties: {
+              balloonContent: "Кутузовский проспект, 88, 2 этаж",
+              iconCaption: "Москва Шоурум"
+            },
+            options: {
+              iconLayout: 'default#imageWithContent',
+              iconImageHref: 'https://liori.ru/wp-content/themes/theme-liori/img/pin.svg',
+              iconImageSize: [76, 76],
+              iconImageOffset: [-54, -54],
+              iconContentOffset: [15, 15],
+            },
+          }, {
+            type: "Feature",
+            id: 4,
+            geometry: {
+              type: "Point",
+              coordinates: [55.675951, 37.913091]
+            },
+            properties: {
+              balloonContent: "Дальняя улица, 6 Люберцы, Московская область",
+              iconCaption: "Люберцы"
+            },
+            options: {
+              iconLayout: 'default#imageWithContent',
+              iconImageHref: 'https://liori.ru/wp-content/themes/theme-liori/img/pin.svg',
+              iconImageSize: [76, 76],
+              iconImageOffset: [-54, -54],
+              iconContentOffset: [15, 15],
+            },
+          }, {
+            type: "Feature",
+            id: 3,
+            geometry: {
+              type: "Point",
+              coordinates: [56.741830, 37.183803]
+            },
+            properties: {
+              balloonContent: "Дачная улица, 1с10 Дубна, Московская область",
+              iconCaption: "Дубна"
+            },
+            options: {
+              iconLayout: 'default#imageWithContent',
+              iconImageHref: 'https://liori.ru/wp-content/themes/theme-liori/img/pin.svg',
+              iconImageSize: [76, 76],
+              iconImageOffset: [-54, -54],
+              iconContentOffset: [15, 15],
+            },
+          }
+        ]
+      });
+      /* 2. Обработка списка и меток */
+      //Клик по метке в карте
+      objectManager.objects.events.add('click', function (e) {
+        var objectId = e.get('objectId');
+        viewObject(objectId);
+      });
+      //Клик в списке
+      [].forEach.call(document.querySelectorAll('[data-objectId]'), function (el) {
+        el.addEventListener('click', function () {
+          var objectId = el.getAttribute("data-objectId");
+          viewObject(objectId);
+        });
+      });
+
+      // Что происходит при выборе метки или варианта из списка
+      function viewObject(objectId) {
+        // Удаляем со всего списка класс active затем добавляем к выбранному
+        for (var object of document.querySelectorAll('[data-objectId]')) {
+          object.classList.remove('map-active');
+        }
+        document.querySelector('[data-objectId="' + objectId + '"]').classList.add('map-active');
+        // Выделяем все метки в синий, затем выбранную в красную
+        /*
+        objectManager.objects.each(function (item) {
+          objectManager.objects.setObjectOptions(item.id, {
+            preset: 'islands#blueIcon'
+          });
+        });
+        objectManager.objects.setObjectOptions(objectId, {
+          preset: 'islands#redDotIcon'
+        });
+        */
+        // Центрирование по метке
+        myMap.setCenter(objectManager.objects.getById(objectId).geometry.coordinates, 10, {
+          checkZoomRange: true
+        });
+      }
+    }
+  })
+})
+
+// ==============================================
+// Горизонтальный скролл
+// ==============================================
+
+const targets = document.querySelectorAll('.jsHorizontalScroll')
+
+if (targets.length > 0) {
+  initTargets();
+
+}
+function initTargets () {
+  for (let index = 0; index < targets.length; index++) {
+    const target = targets[index]
+    target.addEventListener('wheel', event => {
+      const toLeft  = event.deltaY < 0 && target.scrollLeft > 0
+      const toRight = event.deltaY > 0 && target.scrollLeft < target.scrollWidth - target.clientWidth
+
+      if (toLeft || toRight) {
+        event.preventDefault()
+        target.scrollLeft += event.deltaY
+      }
+    })
+}
+}
+
+
+
+
--- app/js/swiper.js
+++ app/js/swiper.js
@@ -1,4 +1,10 @@
-var heroSlider = new Swiper(".heroSlider", {
+/*
+========================================
+      Слайдер на главном экране
+========================================
+*/
+
+const heroSlider = new Swiper(".heroSlider", {
   slidesPerView: 'auto',
 
   navigation: {
@@ -24,3 +30,59 @@ var heroSlider = new Swiper(".heroSlider", {
     },
   },
 });
+
+
+/*
+=====================================================
+      Повышаем качество отделки древесины слайдер
+======================================================
+*/
+
+
+const raiseSwiper = new Swiper(".raiseSwiper", {
+  slidesPerView: 'auto',
+  scrollbar: {
+    el: ".raise__scrollbar",
+    hide: false,
+  },
+
+  navigation: {
+    nextEl: ".raise__next",
+    prevEl: ".raise__prev",
+  },
+
+});
+
+
+/*
+==========================================================================================================
+      Для каждого клиента у нас есть решение слайдер(связанный слайдер)
+===========================================================================================================
+*/
+
+const jsDecisionRight = new Swiper(".jsDecisionRight", { // Правый слайдер
+  slidesPerView: 'auto',
+  spaceBetween: 10,
+  freeMode: true,
+  loop: false,
+  loopedSlides: 1, //Связанный слайдеры (обязательный параметр)
+  watchSlidesProgress: true,
+  watchSlidesVisibility: true,
+  allowTouchMove: false,
+});
+
+const jsDecisionLeft = new Swiper(".jsDecisionLeft", { // Левый слайдер
+  slidesPerView: 1,
+  loop: false,
+  navigation: {
+    nextEl: ".decision__button-next",
+    prevEl: ".decision__button-prev",
+  },
+  thumbs: {
+    swiper: jsDecisionRight,
+  },
+});
+
+
+
+
